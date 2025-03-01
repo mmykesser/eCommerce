e4bfa8d889f4bd802a0258c4d49a6a6131d6574d
Maksym Polinovskyi <mmykesser@gmail.com>
2025-03-01 18:36:26 +0100

    feat(backend): add Product model

---
+++ backend/src/models/Product.ts
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from 'mongoose';
+import { IProduct } from '../types/product.types';
+
+const ProductSchema = new Schema<IProduct>({
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  images: { type: [String], required: true },
+});
+
+export const ProductModel = mongoose.model<IProduct>('Product', ProductSchema);
