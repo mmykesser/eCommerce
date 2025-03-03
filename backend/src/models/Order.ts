72d9fa80fc5be45cbb3aa2785664cff013e545b2
Maksym Polinovskyi <mmykesser@gmail.com>
2025-03-03 21:55:19 +0100

    feat(backend): add Order model

---
+++ backend/src/models/Order.ts
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from 'mongoose';
+import { IOrder } from '../types/order.types';
+
+const OrderSchema = new Schema<IOrder>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    products: [
+      {
+        product: {
+          type: Schema.Types.ObjectId,
+          ref: 'Category',
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+    totalPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    inPostPaczkomat: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const OrderModel = mongoose.model<IOrder>('Order', OrderSchema);
