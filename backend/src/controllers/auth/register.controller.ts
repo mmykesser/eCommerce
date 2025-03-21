cf304408928095122da37a4a8358d86cc5f9e36c
Maksym Polinovskyi <mmykesser@gmail.com>
2025-03-21 22:13:22 +0100

    refactor(backend): update registration controller

--- backend/src/controllers/auth/register.controller.ts
+++ backend/src/controllers/auth/register.controller.ts
@@ -1,17 +1,18 @@
-import { Request, Response } from 'express';
+import { RequestHandler } from 'express';
 import { registerUser } from '../../services/auth.service';
-import { ConflictError } from '../../utils/errors.utils';
+import { ConflictError, ValidationError } from '../../utils/errors.utils';
 
-export const register = async (req: Request, res: Response) => {
+export const register: RequestHandler = async (req, res) => {
   try {
-    const { email, password, name } = req.body;
-    const user = await registerUser(email, password, name);
+    const user = await registerUser(req.body);
     res.status(201).json({
       success: true,
       data: { userId: user._id },
     });
   } catch (error) {
-    if (error instanceof ConflictError) {
+    if (error instanceof ValidationError) {
+      res.status(400).json({ success: false, message: error.message });
+    } else if (error instanceof ConflictError) {
       res.status(409).json({ success: false, message: error.message });
     } else {
       res.status(500).json({ success: false, message: 'Server error' });
