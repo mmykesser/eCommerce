bbbdad9d8adac015e358b58a8bda13c085dc01b5
Maksym Polinovskyi <mmykesser@gmail.com>
2025-03-21 21:21:44 +0100

    refactor(backend): update auth service with RegistrationData type

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,18 +1,16 @@
 import { UserModel } from '../models/User';
 import { IUser } from '../types/user.types';
+import { RegistrationData } from '../types/auth.types';
 import { ConflictError } from '../utils/errors.utils';
 
-export const registerUser = async (
-  email: string,
-  password: string,
-  name: string,
-): Promise<IUser> => {
+export const registerUser = async (data: RegistrationData): Promise<IUser> => {
+  const { email } = data;
   const existingUser = await UserModel.findOne({ email });
   if (existingUser) {
     throw new ConflictError('Email already exists');
   }
 
-  const user = new UserModel({ email, password, name, role: 'user' });
+  const user = new UserModel({ ...data, role: 'user' });
   await user.save();
   return user;
 };
