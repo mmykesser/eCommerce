9dde0863a5f5737305098973451350c4c83f3cf9
Maksym Polinovskyi <mmykesser@gmail.com>
2025-06-04 09:28:39 +0200

    feat(backend): add global error middleware

---
+++ backend/src/middleware/error.middleware.ts
@@ -0,0 +1,25 @@
+import { ErrorRequestHandler } from 'express';
+import { ValidationError, ConflictError } from '../utils/errors.utils';
+
+export const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+  res.setHeader('Content-Type', 'application/json');
+
+  if (err instanceof ConflictError) {
+    return res.status(409).json({
+      success: false,
+      message: err.message,
+    });
+  }
+  if (err instanceof ValidationError) {
+    return res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  }
+  console.error('Unhandled error:', err);
+
+  res.status(500).json({
+    success: false,
+    message: 'Internal server error',
+  });
+};
