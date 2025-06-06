2d8683b12061336a13d6eebbf9bb792566c44de4
Maksym Polinovskyi <mmykesser@gmail.com>
2025-06-06 14:18:49 +0200

    refactor(backend): simplify error handling in register controller

--- backend/src/controllers/auth/register.controller.ts
+++ backend/src/controllers/auth/register.controller.ts
@@ -1,21 +1,14 @@
 import { RequestHandler } from 'express';
 import { registerUser } from '../../services/auth.service';
-import { ConflictError, ValidationError } from '../../utils/errors.utils';
 
-export const register: RequestHandler = async (req, res) => {
+export const register: RequestHandler = async (req, res, next) => {
   try {
     const user = await registerUser(req.body);
     res.status(201).json({
       success: true,
       data: { userId: user._id },
     });
-  } catch (error) {
-    if (error instanceof ValidationError) {
-      res.status(400).json({ success: false, message: error.message });
-    } else if (error instanceof ConflictError) {
-      res.status(409).json({ success: false, message: error.message });
-    } else {
-      res.status(500).json({ success: false, message: 'Server error' });
-    }
+  } catch (err) {
+    next(err);
   }
 };
