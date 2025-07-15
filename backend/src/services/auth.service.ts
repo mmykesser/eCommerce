7c541443c3de1f7a731589d25cec094808e141d7
Maksym Polinovskyi <mmykesser@gmail.com>
2025-07-16 01:36:20 +0200

    refactor(backend): encapsulate auth logic in AuthService class

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,16 +1,41 @@
+import jwt from 'jsonwebtoken';
 import { UserModel } from '../models/User';
-import { IUser } from '../interfaces/models/user.interface';
 import { IRegistrationData } from '../interfaces/dto/auth.interface';
+import {
+  IAuthService,
+  IAuthTokens,
+  PublicUser,
+} from '../interfaces/services/auth.service.interface';
 import { ConflictError } from '../utils/errors.utils';
+import config from '../config/config';
+import { IUser } from '../interfaces/models/user.interface';
 
-export const registerUser = async (data: IRegistrationData): Promise<IUser> => {
-  const { email } = data;
-  const existingUser = await UserModel.findOne({ email });
-  if (existingUser) {
-    throw new ConflictError('Email already exists');
+export class AuthService implements IAuthService {
+  public async register(
+    data: IRegistrationData,
+  ): Promise<{ user: PublicUser; tokens: IAuthTokens }> {
+    const { email } = data;
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+      throw new ConflictError('User with this email already exists');
+    }
+    const newUser = new UserModel(data);
+    const tokens = this._generateTokens({ id: newUser._id });
+    newUser.refreshToken = tokens.refreshToken;
+
+    await newUser.save();
+
+    const userResponse = this._preparePublicUser(newUser);
+    return { user: userResponse, tokens };
+  }
+  private _generateTokens(payload: object): IAuthTokens {
+    const accessToken = jwt.sign(payload, config.jwtSecret, { expiresIn: '15m' });
+    const refreshToken = jwt.sign(payload, config.jwtSecret, { expiresIn: '30d' });
+    return { accessToken, refreshToken };
   }
 
-  const user = new UserModel({ ...data, role: 'user' });
-  await user.save();
-  return user;
-};
+  private _preparePublicUser(user: IUser): PublicUser {
+    const { _id, email, name, role } = user;
+    return { _id, email, name, role };
+  }
+}
