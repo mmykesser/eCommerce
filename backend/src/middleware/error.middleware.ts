0affcd8338a9bfaf75186dd204ebd7ee0fe1027c
Maksym Polinovskyi <mmykesser@gmail.com>
2025-07-25 00:50:24 +0200

    feat(backend): add UnauthorizedError and handler

--- backend/src/middleware/error.middleware.ts
+++ backend/src/middleware/error.middleware.ts
@@ -1,5 +1,5 @@
 import { ErrorRequestHandler } from 'express';
-import { ValidationError, ConflictError } from '../utils/errors.utils';
+import { ValidationError, ConflictError, UnauthorizedError } from '../utils/errors.utils';
 
 export const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
   res.setHeader('Content-Type', 'application/json');
@@ -12,6 +12,12 @@ export const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
   }
   if (err instanceof ValidationError) {
     return res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  }
+  if (err instanceof UnauthorizedError) {
+    return res.status(401).json({
       success: false,
       message: err.message,
     });
