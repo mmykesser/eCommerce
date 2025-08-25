f83fbab40a98dc3abcbc02c357db59a8d9db7d00
Maksym Polinovskyi <mmykesser@gmail.com>
2025-08-25 23:47:42 +0200

    feat(backend): add middleware for authentication

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -5,25 +5,35 @@ import config from '../config/config';
 import { IPayload } from '../interfaces/auth/payload.interface';
 import { UserModel } from '../models/User';
 import { AuthService } from '../services/auth.service';
+import { Error } from 'mongoose';
 
 export const protect: RequestHandler = async (req, _res, next) => {
-  let token;
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-    try {
+  try {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
       token = req.headers.authorization.split(' ')[1];
-
-      const payload = jwt.verify(token, config.jwtSecret) as IPayload;
-
-      const user = await UserModel.findById(payload.id).select('-password');
-      if (!user) {
-        return next(new UnauthorizedError('The user associated with this token no longer exists.'));
-      }
-
-      req.user = AuthService.preparePublicUser(user);
-      return next();
-    } catch (error) {
-      return next(new UnauthorizedError('Not authorized, token failed'));
     }
+    if (!token) {
+      return next(new UnauthorizedError('Not authorized, no token'));
+    }
+    const payload = jwt.verify(token, config.jwtSecret) as IPayload;
+    const user = await UserModel.findById(payload.id).select('-password');
+    if (!user) {
+      return next(new UnauthorizedError('The user associated with this token no longer exists.'));
+    }
+    req.user = AuthService.preparePublicUser(user);
+    next();
+  } catch (err) {
+    console.error('Token verification failed:', err instanceof Error ? err.message : String(err));
+
+    if (err instanceof jwt.TokenExpiredError) {
+      return next(new UnauthorizedError('Token has expired'));
+    }
+
+    if (err instanceof jwt.JsonWebTokenError) {
+      return next(new UnauthorizedError('Invalid token'));
+    }
+    return next(new UnauthorizedError('Not authorized, token failed'));
   }
-  return next(new UnauthorizedError('Not authorized, no token'));
 };
