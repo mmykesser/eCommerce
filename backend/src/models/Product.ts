5638b03ed7696a75608b22a4587840699067a39b
Maksym Polinovskyi <mmykesser@gmail.com>
2025-08-27 18:21:15 +0200

    refactor(backend): improve Product model

--- backend/src/models/Product.ts
+++ backend/src/models/Product.ts
@@ -1,16 +1,22 @@
 import mongoose, { Schema } from 'mongoose';
 import { IProductDocument } from '../interfaces/models/product.interface';
 
-const ProductSchema = new Schema<IProductDocument>({
-  title: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: { type: String, required: true },
-  category: {
-    type: Schema.Types.ObjectId,
-    ref: 'Category',
-    required: true,
+const ProductSchema = new Schema<IProductDocument>(
+  {
+    title: { type: String, required: true, trim: true },
+    price: { type: Number, required: true, min: [0, 'Price cannot be negative'] },
+    description: { type: String, required: true },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    images: { type: [String], required: true },
   },
-  images: { type: [String], required: true },
-});
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
 
 export const ProductModel = mongoose.model<IProductDocument>('Product', ProductSchema);
