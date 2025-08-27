2fd06e078520c8f9dbf85cca81d8ee0de0a80cf0
Maksym Polinovskyi <mmykesser@gmail.com>
2025-08-27 18:44:20 +0200

    feat(backend): add NotFoundError

--- backend/src/utils/errors.utils.ts
+++ backend/src/utils/errors.utils.ts
@@ -18,3 +18,10 @@ export class UnauthorizedError extends Error {
     this.name = 'UnauthorizedError';
   }
 }
+
+export class NotFoundError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'NotFoundError';
+  }
+}
