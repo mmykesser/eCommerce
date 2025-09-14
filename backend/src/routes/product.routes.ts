bf9a2bba5f315131f12daedde95d01034e053c4b
Maksym Polinovskyi <mmykesser@gmail.com>
2025-09-14 14:23:02 +0200

    feat(backend): add check role admin in a route to crate product

--- backend/src/routes/product.routes.ts
+++ backend/src/routes/product.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { ProductController } from '../controllers/product.controller';
-import { protect } from '../middleware/auth.middleware';
+import { protect, authorize } from '../middleware/auth.middleware';
 import { validateCreateProduct } from '../middleware/validation/product.validation';
 
 const router = Router();
@@ -9,6 +9,12 @@ const productController = new ProductController();
 
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
-router.post('/', protect, validateCreateProduct, productController.createProduct);
+router.post(
+  '/',
+  protect,
+  authorize('admin'),
+  validateCreateProduct,
+  productController.createProduct,
+);
 
 export default router;
