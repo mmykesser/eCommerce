d407cf4f7edc4237bd5889ac72331377649e52e8
Maksym Polinovskyi <mmykesser@gmail.com>
2025-09-16 11:58:34 +0200

    feat(backend): add update and delete methods to ProductService

--- backend/src/services/product.service.ts
+++ backend/src/services/product.service.ts
@@ -10,6 +10,7 @@ export class ProductService {
 
   public async findProductById(id: string) {
     const product = await ProductModel.findById(id).populate('category');
+
     if (!product) {
       throw new NotFoundError('Product not found');
     }
@@ -18,9 +19,38 @@ export class ProductService {
 
   public async createProduct(productData: IProduct) {
     const categoryExists = await CategoryModel.findById(productData.category);
+
     if (!categoryExists) {
       throw new NotFoundError('Category ID not found');
     }
     return ProductModel.create(productData);
+  }
+
+  public async updateProduct(productId: string, productData: Partial<IProduct>) {
+    if (productData.category) {
+      const categoryExists = await CategoryModel.findById(productData.category);
+
+      if (!categoryExists) {
+        throw new NotFoundError('Category ID not found');
+      }
+    }
+
+    const updatedProduct = await ProductModel.findByIdAndUpdate(productId, productData, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedProduct) {
+      throw new NotFoundError('Product ID not found');
+    }
+    return updatedProduct;
+  }
+
+  public async deleteProduct(productId: string): Promise<void> {
+    const deleteProduct = await ProductModel.findByIdAndDelete(productId);
+
+    if (!deleteProduct) {
+      throw new NotFoundError('Product ID not found');
+    }
   }
 }
