ea32bb68449722b035dbddb7e3b8b2d67cbddbd8
Maksym Polinovskyi <mmykesser@gmail.com>
2025-12-02 20:08:24 +0100

    feat(backend): add endpoint to get orders

--- backend/src/routes/order.routes.ts
+++ backend/src/routes/order.routes.ts
@@ -6,6 +6,9 @@ import { validateCreateOrder } from '../middleware/validation/order.validation';
 const router = Router();
 const orderController = new OrderController();
 
-router.route('/').post(protect, validateCreateOrder, orderController.createOrder);
+router
+  .route('/')
+  .post(protect, validateCreateOrder, orderController.createOrder)
+  .get(protect, orderController.getOrders);
 
 export default router;
