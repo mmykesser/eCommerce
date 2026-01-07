ab734e0bd4e32f01c727e9621fe370a5f9dec3f0
Maksym Polinovskyi <mmykesser@gmail.com>
2026-01-07 11:15:23 +0100

    feat(backend): add CartController

---
+++ backend/src/controllers/cart.controller.ts
@@ -0,0 +1,61 @@
+import { RequestHandler } from 'express';
+import { CartService } from '../services/cart.service';
+import { UnauthorizedError } from '../utils/errors.utils';
+import { IAddToCartData } from '../interfaces/dto/cart.interface';
+
+export class CartController {
+  private cartService = new CartService();
+
+  public getCart: RequestHandler = async (req, res, next) => {
+    try {
+      if (!req.user) {
+        return next(new UnauthorizedError('Authentication is required to get cart'));
+      }
+      const cart = await this.cartService.getCart(req.user._id);
+
+      res.status(200).json({
+        success: true,
+        data: cart,
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  public addToCart: RequestHandler = async (req, res, next) => {
+    try {
+      if (!req.user) {
+        return next(new UnauthorizedError('Authentication is required to add to cart'));
+      }
+
+      const cartData: IAddToCartData = req.body;
+      const { productId, quantity } = cartData;
+      const cart = await this.cartService.addToCart(req.user._id, productId, quantity);
+
+      res.status(200).json({
+        success: true,
+        data: cart,
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  public removeFromCart: RequestHandler = async (req, res, next) => {
+    try {
+      if (!req.user) {
+        return next(new UnauthorizedError('Authentication is required to remove from cart'));
+      }
+
+      const { productId } = req.params;
+      const cart = await this.cartService.removeFromCart(req.user._id, productId);
+
+      res.status(200).json({
+        success: true,
+        data: cart,
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
+}
