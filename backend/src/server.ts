e43a4dc19a449867d03f5f77f18c161951458da4
Maksym Polinovskyi <mmykesser@gmail.com>
2026-03-28 23:59:58 +0100

    chore(backend): add and configure cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express, { Express } from 'express';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import config from './config/config';
 import connectDB from './database/db';
@@ -12,6 +13,14 @@ import inpostRoutes from './routes/inpost.routes';
 import { errorHandler } from './middleware/error.middleware';
 
 const app: Express = express();
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
