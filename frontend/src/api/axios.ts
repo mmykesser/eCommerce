7acfef18bd0beab8fcead1ba8925f2173a66b9b9
Maksym Polinovskyi <mmykesser@gmail.com>
2026-08-12 10:44:16 +0200

    feat(frontend): add request interceptor for authorization header

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -8,4 +8,14 @@ const $api = axios.create({
   },
 });
 
+$api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken');
+
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default $api;
